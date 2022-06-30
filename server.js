57effd38c85b8407b6b343db6f25cb1dd7bf7e84
zenyoo <108466975+zenyoo@users.noreply.github.com>
2022-06-30 19:53:12 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express")
+
+const server = express()
+
+server.all("/", (req, res) => {
+
+  res.send('Please connect me into a hosting website to enable 24/7 hosting. zenyoo#3866')
+
+})const express = require("express")
+
+const server = express()
+
+server.all("/", (req, res) => {
+
+  res.send('Please connect me into a hosting website to enable 24/7 hosting. zenyoo#3866')
+
+})
+
+function keepAlive() {
+
+  server.listen(3000, () => {
+
+    console.log("Creator: ItzNexus")
+
+  })
+
+}
+
+module.exports = keepAlive
+
+function keepAlive() {
+
+  server.listen(3000, () => {
+
+    console.log("Creator: ItzNexus")
+
+  })
+
+}
+
+module.exports = keepAlive
